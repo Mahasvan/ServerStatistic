7b51bde9aaef09ce239a0772642084322e9ee367
Mahasvan <mahasvan@gmail.com>
2025-08-28 16:25:23 +0530

    Initial Commit

---
+++ Statistic/StatisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticApp.swift
+//  Statistic
+//
+//  Created by Mahasvan Mohan on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct StatisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatisticTests/StatisticTests.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticTests.swift
+//  StatisticTests
+//
+//  Created by Mahasvan Mohan on 28/08/25.
+//
+
+import Testing
+@testable import Statistic
+
+struct StatisticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
